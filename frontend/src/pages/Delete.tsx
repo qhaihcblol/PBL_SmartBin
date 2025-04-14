8fbf45d1d71fb14c5abd9a4f2debdc703bec8fd0
qhaihcblol <102220013@sv1.dut.udn.vn>
2025-04-14 22:16:51 +0700

    Initial commit

---
+++ frontend/src/pages/Delete.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Delete: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Delete Page!</h1>
+        </div>
+    );
+}
+export default Delete;
---
+++ frontend/src/pages/Edit.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Edit: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Edit Page!</h1>
+        </div>
+    );
+}
+export default Edit;
